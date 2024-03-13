39ac2b525d6403734e5edcbaad59eb475fbe4505
minirop <minirop@hotmail.com>
2024-03-13 21:44:46 +0100

    moved value and stdlib into their own files

---
+++ src/core.rs
@@ -0,0 +1,618 @@
+use crate::Function;
+use crate::Fiber;
+use crate::Chunk;
+use crate::Opcode;
+use std::cell::RefCell;
+use std::rc::Rc;
+use crate::Class;
+use std::collections::HashMap;
+use crate::NativeFunction;
+use crate::Value;
+use crate::Vm;
+
+pub fn register(vm: &mut Vm) {
+    load_system(vm);
+    load_sequence(vm);
+    load_bool(vm);
+    load_string(vm);
+    load_list(vm);
+    load_fn(vm);
+    load_fiber(vm);
+}
+
+macro_rules! generate_nl_writer {
+    (true) => {
+        println!("");
+    };
+    (false) => {
+    };
+}
+
+macro_rules! generate_writer {
+    ($nats:ident, $name:ident, $nl:tt, $args_count:literal) => {{
+        let args_ph = if $args_count > 0 { format!("_{}", ",_".repeat($args_count - 1)) } else { "".to_string() };
+        let name_ph: Rc<str> = format!("{}({args_ph})", stringify!($name)).into();
+
+        $nats.insert(name_ph.clone(), NativeFunction {
+            name: name_ph.clone(),
+            arity: $args_count,
+            code: |vm| {
+                let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+                let base = fiber.stack.len() - $args_count;
+                for i in 0..$args_count {
+                    let a = fiber.stack.get(base + i).unwrap();
+                    print!("{a}");
+                }
+
+                generate_nl_writer!($nl);
+
+                for _ in 0..$args_count {
+                    fiber.pop();
+                }
+                fiber.pop();
+                fiber.push_null();
+            },
+        });
+    }}
+}
+
+fn load_system(vm: &mut Vm) {
+    let mut obj_nats = HashMap::new();
+    obj_nats.insert("name".into(), NativeFunction {
+        name: "name".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.stack.pop().unwrap();
+
+            let Value::Class(class) = top else {
+                fiber.error = Some(format!("Can't call name on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+
+            fiber.pop();
+            fiber.stack.push(Value::String(class.borrow().name.clone()));
+        },
+    });
+    obj_nats.insert("supertype".into(), NativeFunction {
+        name: "supertype".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.stack.pop().unwrap();
+            
+            let Value::Class(class) = top else {
+                fiber.error = Some(format!("Can't call supertype on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+
+            fiber.pop();
+            if let Some(supertype) = &class.borrow().supertype {
+                fiber.stack.push(Value::String(supertype.clone()));
+            } else {
+                fiber.push_null();
+            };
+        },
+    });
+    let object_class = Class {
+        name: "Object".into(),
+        supertype: None,
+        fields: HashMap::new(),
+        functions: HashMap::new(),
+        natives: obj_nats,
+    };
+
+    let mut nats = HashMap::new();
+
+    generate_writer!(nats, print, true, 0);
+    generate_writer!(nats, print, true, 1);
+    generate_writer!(nats, print, true, 2);
+    generate_writer!(nats, print, true, 3);
+    generate_writer!(nats, print, true, 4);
+    generate_writer!(nats, print, true, 5);
+    generate_writer!(nats, print, true, 6);
+    generate_writer!(nats, print, true, 7);
+    generate_writer!(nats, print, true, 8);
+    generate_writer!(nats, print, true, 9);
+    generate_writer!(nats, print, true, 10);
+    generate_writer!(nats, print, true, 11);
+    generate_writer!(nats, print, true, 12);
+    generate_writer!(nats, print, true, 13);
+    generate_writer!(nats, print, true, 14);
+    generate_writer!(nats, print, true, 15);
+    generate_writer!(nats, print, true, 16);
+
+    generate_writer!(nats, write, false, 1);
+    generate_writer!(nats, write, false, 2);
+    generate_writer!(nats, write, false, 3);
+    generate_writer!(nats, write, false, 4);
+    generate_writer!(nats, write, false, 5);
+    generate_writer!(nats, write, false, 6);
+    generate_writer!(nats, write, false, 7);
+    generate_writer!(nats, write, false, 8);
+    generate_writer!(nats, write, false, 9);
+    generate_writer!(nats, write, false, 10);
+    generate_writer!(nats, write, false, 11);
+    generate_writer!(nats, write, false, 12);
+    generate_writer!(nats, write, false, 13);
+    generate_writer!(nats, write, false, 14);
+    generate_writer!(nats, write, false, 15);
+    generate_writer!(nats, write, false, 16);
+
+    let system_class = Class {
+        name: "System".into(),
+        supertype: Some("Object".into()),
+        fields: HashMap::new(),
+        functions: HashMap::new(),
+        natives: nats,
+    };
+
+    vm.variables.insert("Object".into(), Value::Class(Rc::new(RefCell::new(object_class))));
+    vm.variables.insert("System".into(), Value::Class(Rc::new(RefCell::new(system_class))));
+}
+
+fn load_bool(vm: &mut Vm) {
+    let mut nats = HashMap::new();
+    nats.insert("!".into(), NativeFunction {
+        name: "!".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.stack.pop().unwrap();
+            let Value::Bool(b) = top else {
+                fiber.error = Some(format!("Can't call ! on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+
+            fiber.stack.push(Value::Bool(!b));
+        },
+    });
+    let bool_class = Class {
+        name: "Bool".into(),
+        supertype: Some("Object".into()),
+        fields: HashMap::new(),
+        functions: HashMap::new(),
+        natives: nats,
+    };
+
+    vm.variables.insert("Bool".into(), Value::Class(Rc::new(RefCell::new(bool_class))));
+}
+
+fn load_string(vm: &mut Vm) {
+    let mut nats = HashMap::new();
+    nats.insert("count".into(), NativeFunction {
+        name: "count".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.stack.pop().unwrap();
+            let Value::String(string) = top else {
+                fiber.error = Some(format!("Can't call String.count on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+
+            fiber.stack.push(Value::Integer(string.len() as i32));
+        },
+    });
+    let string_class = Class {
+        name: "String".into(),
+        supertype: Some("Object".into()),
+        fields: HashMap::new(),
+        functions: HashMap::new(),
+        natives: nats,
+    };
+
+    vm.variables.insert("String".into(), Value::Class(Rc::new(RefCell::new(string_class))));
+}
+
+fn load_sequence(vm: &mut Vm) {
+    let mut funcs = HashMap::new();
+
+    let mut code = Chunk::new();
+    code.code.push(Opcode::LoadLocalVar(0));
+    code.code.push(Opcode::LoadLocalVar(2));
+    code.code.push(Opcode::Call("iterate(_)".into(), 1));
+    code.code.push(Opcode::StoreLocalVar(2));
+    code.code.push(Opcode::Pop);
+    code.code.push(Opcode::LoadLocalVar(2));
+    code.code.push(Opcode::Not);
+    code.code.push(Opcode::JumpIf(7));
+    code.code.push(Opcode::LoadLocalVar(1));
+    code.code.push(Opcode::LoadLocalVar(0));
+    code.code.push(Opcode::LoadLocalVar(2));
+    code.code.push(Opcode::Call("iteratorValue(_)".into(), 1));
+    code.code.push(Opcode::Call("call(_)".into(), 1));
+    code.code.push(Opcode::Pop);
+    code.code.push(Opcode::Loop(13));
+    code.code.push(Opcode::Null);
+    code.code.push(Opcode::Return);
+
+    funcs.insert("each(_)".into(), Function {
+        name: "each(_)".into(),
+        arity: 1,
+        locals: 1,
+        code,
+    });
+
+    let nats = HashMap::new();
+
+    let sequence_class = Class {
+        name: "Sequence".into(),
+        supertype: Some("Object".into()),
+        fields: HashMap::new(),
+        functions: funcs,
+        natives: nats,
+    };
+
+    vm.variables.insert("Sequence".into(), Value::Class(Rc::new(RefCell::new(sequence_class))));
+}
+
+fn load_list(vm: &mut Vm) {
+    let funcs = HashMap::new();
+
+    let mut nats = HashMap::new();
+    nats.insert("new()".into(), NativeFunction {
+        name: "new()".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            fiber.stack.pop();
+            fiber.stack.push(Value::List(Vec::new()));
+        },
+    });
+
+    nats.insert("add(_)".into(), NativeFunction {
+        name: "add(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let index = fiber.stack.len() - 2;
+            let arg = fiber.stack.pop().unwrap();
+
+            match fiber.stack.get_mut(index).unwrap() {
+                Value::List(ref mut list) => {
+                    (*list).push(arg);
+                },
+                _ => todo!(),
+            };
+
+            // keep the list at the top of the stack
+        },
+    });
+
+    nats.insert("iterate(_)".into(), NativeFunction {
+        name: "iterate(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let arg = fiber.pop();
+            let list = fiber.pop();
+
+            let Value::List(list) = list else {
+                todo!();
+            };
+
+            let next = match arg {
+                Value::Null => 0,
+                Value::Integer(i) => i + 1,
+                _ => panic!("debug: {:?}", arg),
+            };
+
+            if next < 0 {
+                panic!("no negative iterator");
+            }
+
+            if (next as usize) < list.len() {
+                fiber.stack.push(Value::Integer(next));
+            } else {
+                fiber.push_bool(false);
+            }
+        },
+    });
+
+    nats.insert("iteratorValue(_)".into(), NativeFunction {
+        name: "iteratorValue(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let arg = fiber.pop();
+            let list = fiber.pop();
+
+            let Value::List(list) = list else {
+                todo!();
+            };
+
+            let Value::Integer(index) = arg else {
+                todo!();
+            };
+
+            if index < 0 {
+                panic!("no negative iterator");
+            }
+
+            let index = index as usize;
+
+            if index < list.len() {
+                let val = list[index].clone();
+                fiber.stack.push(val);
+            } else {
+                fiber.push_null();
+            }
+        },
+    });
+
+    nats.insert("count".into(), NativeFunction {
+        name: "count".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.stack.pop().unwrap();
+            let Value::List(list) = top else {
+                fiber.error = Some(format!("Can't call List.count on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+            fiber.stack.push(Value::Integer(list.len() as i32));
+        },
+    });
+
+    let list_class = Class {
+        name: "List".into(),
+        supertype: Some("Sequence".into()),
+        fields: HashMap::new(),
+        functions: funcs,
+        natives: nats,
+    };
+
+    vm.variables.insert("List".into(), Value::Class(Rc::new(RefCell::new(list_class))));
+}
+
+fn load_fn(vm: &mut Vm) {
+    let mut nats = HashMap::new();
+    nats.insert("new(_)".into(), NativeFunction {
+        name: "new(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.stack.last().unwrap();
+
+            match top {
+                Value::Closure(_) => {},
+                _ => {
+                    fiber.error = Some(format!("Fn.new(_) only accepts functions. Got {:?}", top).into());
+                    fiber.push_null();
+                    return;
+                },
+            };
+        },
+    });
+
+    nats.insert("call(_)".into(), NativeFunction {
+        name: "call(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.stack.pop().unwrap();
+
+            let function = match fiber.stack.pop().unwrap() {
+                Value::Closure(function) => function,
+                _ => {
+                    fiber.error = Some(format!("Wanted 'Fn', got {:?}.", top).into());
+                    fiber.push_null();
+                    return;
+                },
+            };
+
+            let len = fiber.stack.len();
+            fiber.push_null();
+            fiber.stack.push(top);
+            fiber.stackbase.push(len);
+            fiber.callstack.push(function);
+        },
+    });
+
+    nats.insert("call()".into(), NativeFunction {
+        name: "call()".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.stack.pop().unwrap();
+
+            let function = match top {
+                Value::Closure(function) => function,
+                _ => {
+                    fiber.error = Some(format!("Wanted 'Fn', got {:?}.", top).into());
+                    fiber.push_null();
+                    return;
+                },
+            };
+
+            let len = fiber.stack.len();
+            fiber.stackbase.push(len - 1);
+            fiber.callstack.push(function);
+        },
+    });
+
+    let fn_class = Class {
+        name: "Fn".into(),
+        supertype: Some("Object".into()),
+        fields: HashMap::new(),
+        functions: HashMap::new(),
+        natives: nats,
+    };
+    vm.variables.insert("Fn".into(), Value::Class(Rc::new(RefCell::new(fn_class))));
+}
+
+fn load_fiber(vm: &mut Vm) {
+    let mut nats = HashMap::new();
+    nats.insert("new(_)".into(), NativeFunction {
+        name: "new(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.stack.pop().unwrap();
+            let Value::Closure(function) = top else {
+                fiber.error = Some(format!("Can't call Fiber.new(_) on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+            fiber.pop(); // pops "Fiber" class
+
+            let mut new_fiber = Fiber::new();
+            new_fiber.push(Value::Closure(function.clone()));
+            new_fiber.push_callframe(function.clone());
+            fiber.push(Value::Fiber(Rc::new(RefCell::new(new_fiber))));
+        },
+    });
+
+    nats.insert("call(_)".into(), NativeFunction {
+        name: "call(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let val = fiber.pop();
+            let top = fiber.pop();
+
+            let Value::Fiber(new_fiber) = top else {
+                fiber.error = Some(format!("Can't call Fiber.call(_) on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+            drop(fiber);
+
+            new_fiber.borrow_mut().push(val);
+            vm.fibers.push(new_fiber);
+        },
+    });
+
+    nats.insert("call()".into(), NativeFunction {
+        name: "call()".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.pop();
+
+            let Value::Fiber(new_fiber) = top else {
+                fiber.error = Some(format!("Can't call Fiber.call() on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+            drop(fiber);
+
+            new_fiber.borrow_mut().push_null();
+            vm.fibers.push(new_fiber);
+        },
+    });
+
+    nats.insert("try(_)".into(), NativeFunction {
+        name: "try(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let val = fiber.pop();
+            let top = fiber.pop();
+
+            let Value::Fiber(new_fiber) = top else {
+                fiber.error = Some(format!("Can't call Fiber.try(_) on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+            drop(fiber);
+
+            new_fiber.borrow_mut().push(val);
+            new_fiber.borrow_mut().trying = true;
+            vm.fibers.push(new_fiber);
+        },
+    });
+
+    nats.insert("try()".into(), NativeFunction {
+        name: "try()".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.pop();
+
+            let Value::Fiber(new_fiber) = top else {
+                fiber.error = Some(format!("Can't call Fiber.try() on {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+            drop(fiber);
+
+            new_fiber.borrow_mut().push_null();
+            new_fiber.borrow_mut().trying = true;
+            vm.fibers.push(new_fiber);
+        },
+    });
+
+    nats.insert("yield(_)".into(), NativeFunction {
+        name: "yield(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.pop();
+            fiber.pop();
+            drop(fiber);
+
+            vm.fibers.pop();
+
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            fiber.push(top);
+        },
+    });
+
+    nats.insert("abort(_)".into(), NativeFunction {
+        name: "abort(_)".into(),
+        arity: 1,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.pop();
+            fiber.pop();
+
+            match top {
+                Value::Null => {},
+                Value::String(s) => fiber.error = Some(s),
+                _ => {
+                    fiber.error = Some(format!("Invalid value given to Fiber.abort(_). Null or a string. Got {:?}.", top).into());
+                    fiber.push_null();
+                    return;
+                }
+            };
+            
+            fiber.push_null();
+        },
+    });
+
+    nats.insert("isDone".into(), NativeFunction {
+        name: "isDone".into(),
+        arity: 0,
+        code: |vm| {
+            let mut fiber = vm.fibers.last_mut().unwrap().borrow_mut();
+            let top = fiber.pop();
+            let Value::Fiber(new_fiber) = top else {
+                fiber.error = Some(format!("Fiber.isDone expects a fiber. Got {:?}.", top).into());
+                fiber.push_null();
+                return;
+            };
+
+            if new_fiber.borrow().callstack.len() == 0 {
+                fiber.push_bool(true);
+            } else {
+                fiber.push_bool(false);
+            }
+        },
+    });
+
+    let fiber_class = Class {
+        name: "Fiber".into(),
+        supertype: Some("Object".into()),
+        fields: HashMap::new(),
+        functions: HashMap::new(),
+        natives: nats,
+    };
+    vm.variables.insert("Fiber".into(), Value::Class(Rc::new(RefCell::new(fiber_class))));
+}
---
+++ src/value.rs
@@ -0,0 +1,234 @@
+
+use std::fmt;
+use std::cmp::Ordering;
+use std::ops::*;
+use crate::Fiber;
+use crate::Opcode;
+use crate::Function;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use std::collections::HashMap;
+
+use crate::NativeFunction;
+
+#[derive(Debug, Clone)]
+pub struct Class {
+    pub name: Rc<str>,
+    pub supertype: Option<Rc<str>>,
+    pub fields: HashMap<Rc<str>, Vec<Opcode>>,
+    pub functions: HashMap<Rc<str>, Function>,
+    pub natives: HashMap<Rc<str>, NativeFunction>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Object {
+    pub class: Rc<str>,
+    pub fields: HashMap<Rc<str>, Value>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Value {
+    Null,
+    Bool(bool),
+    Integer(i32),
+    Float(f32),
+    String(Rc<str>),
+    Class(Rc<RefCell<Class>>),
+    Object(Rc<RefCell<Object>>),
+    List(Vec<Value>),
+    Closure(Function),
+    Fiber(Rc<RefCell<Fiber>>),
+}
+
+impl Add for Value {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        match self {
+            Value::Integer(i) => {
+                match other {
+                    Value::Integer(i2) => Value::Integer(i + i2),
+                    Value::Float(f) => Value::Float(i as f32 + f),
+                    Value::String(string) => Value::String(format!("{i}{string}").into()),
+                    _ => panic!("{} + {}", self, other),
+                }
+            },
+            Value::Float(f) => {
+                match other {
+                    Value::Integer(i) => Value::Float(f + i as f32),
+                    Value::Float(f2) => Value::Float(f + f2),
+                    Value::String(string) => Value::String(format!("{f}{string}").into()),
+                    _ => panic!("{} + {}", self, other),
+                }
+            },
+            Value::String(string) => {
+                Value::String(format!("{string}{other}").into())
+            },
+            _ => match other {
+                Value::String(string) => Value::String(format!("{self}{string}").into()),
+                _ => panic!("{} + {}", self, other)
+            },
+        }
+    }
+}
+
+impl Sub for Value {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        match self {
+            Value::Integer(i) => {
+                match other {
+                    Value::Integer(i2) => Value::Integer(i - i2),
+                    Value::Float(f) => Value::Float(i as f32 - f),
+                    _ => panic!("{} - {}", self, other),
+                }
+            },
+            Value::Float(f) => {
+                match other {
+                    Value::Integer(i) => Value::Float(f - i as f32),
+                    Value::Float(f2) => Value::Float(f - f2),
+                    _ => panic!("{} - {}", self, other),
+                }
+            },
+            _ => panic!("{} - {}", self, other),
+        }
+    }
+}
+
+impl Mul for Value {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        match self {
+            Value::Integer(i) => {
+                match other {
+                    Value::Integer(i2) => Value::Integer(i * i2),
+                    Value::Float(f) => Value::Float(i as f32 * f),
+                    _ => panic!("{} - {}", self, other),
+                }
+            },
+            Value::Float(f) => {
+                match other {
+                    Value::Integer(i) => Value::Float(f * i as f32),
+                    Value::Float(f2) => Value::Float(f * f2),
+                    _ => panic!("{} * {}", self, other),
+                }
+            },
+            Value::String(string) => {
+                match other {
+                    Value::Integer(i) => Value::String(format!("{}", string.repeat(i as usize)).into()),
+                    _ => panic!("'{}' + '{}'", string, other),
+                }
+            },
+            _ => panic!("{} * {}", self, other),
+        }
+    }
+}
+
+impl Div for Value {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self {
+        match self {
+            Value::Integer(i) => {
+                match other {
+                    Value::Integer(i2) => Value::Integer(i / i2),
+                    Value::Float(f) => Value::Float(i as f32 / f),
+                    _ => panic!("{} / {}", self, other),
+                }
+            },
+            Value::Float(f) => {
+                match other {
+                    Value::Integer(i) => Value::Float(f / i as f32),
+                    Value::Float(f2) => Value::Float(f / f2),
+                    _ => panic!("{} / {}", self, other),
+                }
+            },
+            _ => panic!("{} / {}", self, other),
+        }
+    }
+}
+
+impl Not for Value {
+    type Output = Self;
+
+    fn not(self) -> Self {
+        match self {
+            Value::Bool(b) => Value::Bool(!b),
+            Value::Null => Value::Bool(true),
+            _ => Value::Bool(false),
+        }
+    }
+}
+
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        if std::mem::discriminant(self) != std::mem::discriminant(other) {
+            return false;
+        }
+
+        match (self, other) {
+            (Value::Integer(l), Value::Integer(r)) => l == r,
+            (Value::Float(l), Value::Float(r)) => l == r,
+            (Value::Bool(l), Value::Bool(r)) => l == r,
+            (Value::String(l), Value::String(r)) => l == r,
+            (Value::Null, Value::Null) => true,
+            _ => panic!("{} == {}", self, other),
+        }
+    }
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if std::mem::discriminant(self) != std::mem::discriminant(other) {
+            return None;
+        }
+
+        match (self, other) {
+            (Value::Integer(l), Value::Integer(r)) => l.partial_cmp(r),
+            (Value::Float(l), Value::Float(r)) => l.partial_cmp(r),
+            (Value::Bool(l), Value::Bool(r)) => l.partial_cmp(r),
+            (Value::String(l), Value::String(r)) => l.partial_cmp(r),
+            (Value::Null, Value::Null) => Some(Ordering::Equal),
+            _ => panic!("{:?} cmp {:?}", self, other),
+        }
+    }
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::Float(float) => write!(f, "{}", float),
+            Value::Bool(b) => write!(f, "{}", b),
+            Value::Null => write!(f, "null"),
+            Value::String(s) => write!(f, "{}", s),
+            Value::Integer(i) => write!(f, "{}", i),
+            Value::Class(c) => write!(f, "<class '{}'>", c.borrow().name),
+            Value::Object(o) => write!(f, "<object of type '{}'>", o.borrow().class),
+            Value::List(list) => {
+                write!(f, "[").unwrap();
+                for i in 0..list.len() {
+                    if i > 0 { write!(f, ", ").unwrap(); }
+                    write!(f, "{}", list[i]).unwrap();
+                }
+                write!(f, "]")
+            },
+            Value::Closure(_) => write!(f, "<closure>"),
+            Value::Fiber(_) => write!(f, "<fiber>"),
+        }
+    }
+}
+
+impl Neg for Value {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        match self {
+            Value::Float(f) => Value::Float(-f),
+            Value::Integer(i) => Value::Integer(-i),
+            _ => panic!("Can't negate {}", self),
+        }
+    }
+}
